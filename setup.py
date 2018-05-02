b01b8dd4ca7afa8295ad151f070957248beedc7f
mireleschavezv <mireleschavezv@semantic-web.com>
2018-05-02 12:23:41 +0200

    added Setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
+requirements = open('requirements.txt', 'r').read().split()
+requirements.append("https://github.com/artreven/pp_api/tarball#egg=pp_api")
+
 setup(
     name='pp_vectorizer',
     version='0.1dev',
     packages=['pp_vectorizer'],
     license='Apache2',
-    requires=open('requirements.txt', 'r').read().split()
+    requires=[x for x in requirements if x[0] != '-']
 )
