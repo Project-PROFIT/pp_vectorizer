efa433721f19a239aec680be5cfa279531d28a3a
mireleschavezv <mireleschavezv@semantic-web.com>
2018-05-02 14:33:08 +0200

    added Setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
-requirements = open('requirements.txt', 'r').read().split()
-requirements.append("https://github.com/artreven/pp_api/tarball#egg=pp_api")
+
+requirements = open('requirements.txt', 'r').read().split("\n")
+requirements = [x for x in requirements if (len(x)>0) and  (x[0] != '-') and ("+" not in x)]
+requirements = [x.replace("python-","") for x in requirements]
+print(requirements)
+dependencies = ["https://github.com/artreven/pp_api/tarball/master#egg=pp_api"]
 
 setup(
     name='pp_vectorizer',
     version='0.1dev',
+    description='text vectorized based on PoolParty API',
     packages=['pp_vectorizer'],
     license='Apache2',
-    requires=[x for x in requirements if x[0] != '-']
+    install_requires=requirements,
+    dependency_links=dependencies
 )
